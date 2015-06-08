529b79d633d0034962e767b8a59c1efcfac41256
Zac Brachmanis <zbrachmanis@san-mac4119.merchant.local>
2015-06-08 15:01:42 -0700

    Optimizing...

--- includes/IntervalMakerClass.php
+++ includes/IntervalMakerClass.php
@@ -1,89 +1,80 @@
 <?php
 date_default_timezone_set('America/Los_Angeles');
 /**
-* IntervalMaker
-*/
+ * IntervalMaker
+ */
 class IntervalMaker {
 
-    public function createDates($startdate, $finishdate, $desired_interval, $format = 'm/d/Y H:i:s', $timezone = 'PST') {
-        $times = array();
-		
-        $interval = $this->setInterval($desired_interval);
+	public function createDates($startdate, $finishdate, $desired_interval, $format = 'm/d/Y H:i:s', $timezone = 'PST') {
+		$interval = $this->setInterval($desired_interval);
 
-        //DETERMINES INITIAL NEXT DATE, THIS WILL BE RESET IN THE WHILE LOOP
-        $nextdate = strtotime($interval, $startdate);
+		//DETERMINES INITIAL NEXT DATE, THIS WILL BE RESET IN THE WHILE LOOP
+		$nextdate = strtotime($interval, $startdate);
 
-        if ($startdate !== $finishdate) {
-            //ADDS FIRST TWO DATES
-            $times = array(date($format, $startdate) . " " . $timezone, date($format, $nextdate) . " " . $timezone);
-            $nextdate2 = '';
+		if ($startdate === $finishdate || $nextdate > $finishdate) {
+			return array(date($format, $startdate) . ' ' . $timezone);
+		} else {
+			//ADDS FIRST TWO DATES
+			$times = array(date($format, $startdate) . ' ' . $timezone, date($format, $nextdate) . ' ' . $timezone);
+			$nextdate2 = '';
 
-            if ($nextdate > $finishdate) {
-                return array(date($format, $startdate) . " " . $timezone);
-            } else {
-                //ADDS THE REST OF THE DATES
-                while ($nextdate2 < $finishdate) {
-                    $nextdate = strtotime($interval, $nextdate);
-                    $nextdate2 = strtotime($interval, $nextdate);
+			//ADDS THE REST OF THE DATES
+			while ($nextdate2 < $finishdate) {
+				$nextdate = strtotime($interval, $nextdate);
+				$nextdate2 = strtotime($interval, $nextdate);
 
-                    $times[] = date($format, $nextdate) . " " . $timezone;
-                }
-            }
+				$times[] = date($format, $nextdate) . ' ' . $timezone;
+			}
+            return $times;
+		}
+	}
 
+	public function createTimestamps($startdate, $finishdate, $desired_interval) {
+		$interval = $this->setInterval($desired_interval);
+
+		//DETERMINES INITIAL NEXT DATE, THIS WILL BE RESET IN THE WHILE LOOP
+		$nextdate = strtotime($interval, $startdate);
+
+        if ($startdate === $finishdate || $nextdate > $finishdate) {
+            return array(array($startdate, $finishdate));
         } else {
-            $times[] = array($startdate, $finishdate);
-        }
+			//ADDS FIRST TWO DATES
+			$timestamps[] = array($startdate, $nextdate);
+			$timestamps[] = array($nextdate, strtotime($interval, $nextdate));
+			$nextdate2 = '';
+			
+            //ADDS THE REST OF THE DATES
+			while ($nextdate2 < $finishdate) {
+				$nextdate = strtotime($interval, $nextdate);
+				$nextdate2 = strtotime($interval, $nextdate);
 
-        return $times;
-    }
+				$timestamps[] = array($nextdate, $nextdate2);
+			}
+            return $timestamps;
+		}
+	}
 
-    public function createTimestamps($startdate, $finishdate, $desired_interval) {
-        $timestamps = array();
-
-        $interval = $this->setInterval($desired_interval);
-
-        //DETERMINES INITIAL NEXT DATE, THIS WILL BE RESET IN THE WHILE LOOP
-        $nextdate = strtotime($interval, $startdate);
-
-        if ($startdate !== $finishdate) {
-            //ADDS FIRST TWO DATES
-            $timestamps[] = array($startdate, $nextdate);
-            $timestamps[] = array($nextdate, strtotime($interval, $nextdate));
-            $nextdate2 = '';
-
-            if ($nextdate > $finishdate) {
-                return array(array($startdate, $finishdate));
-            } else {
-                //ADDS THE REST OF THE DATES
-                while ($nextdate2 < $finishdate) {
-                    $nextdate = strtotime($interval, $nextdate);
-                    $nextdate2 = strtotime($interval, $nextdate);
-
-                    $timestamps[] = array($nextdate, $nextdate2);
-                }
-            }
-
-        } else {
-            $timestamps[] = array($startdate, $finishdate);
-        }
-
-        return $timestamps;
-    }
-
-    protected function setInterval($desired_interval) {
-        switch ($desired_interval) {
-            case 'hours':
-                return '+1 hour';
-            case 'days':
-                return '+1 day';
-            case 'weeks':
-                return '+1 week';
-            case 'months':
-                return '+1 month';
-            case 'years':
-                return '+1 year';
-            default:
-                return null;
-        }
-    }
+	protected function setInterval($desired_interval) {
+		switch ($desired_interval) {
+			case 'hours':
+				return '+1 hour';
+			case 'days':
+				return '+1 day';
+			case 'weeks':
+				return '+1 week';
+			case 'months':
+				return '+1 month';
+			case 'years':
+				return '+1 year';
+			default:
+				return null;
+		}
+	}
+	function __construct() {
+	}
 }
+
+$timemaker = new IntervalMaker();
+$dates = $timemaker->createDates(1388534400, 1433788898, 'months');
+
+var_dump($dates);
